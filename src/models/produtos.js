a11d4d1f7b63ee0165148d64990fbd61d8c5a1c8
Aline Mizumukai <aline.mizumukai@mag-it.com.br>
2022-11-14 18:45:57 -0300

    Criando a model Produtos

---
+++ src/models/produtos.js
@@ -0,0 +1,32 @@
+const db = require("../database");
+const { DataTypes } = require('sequelize'); //auxilia no de para dos datatypes do banco para JS
+
+const Produtos = db.define("Produtos", {
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    nome:{
+        type: DataTypes.STRING,
+    },
+    preco:{
+        type: DataTypes.FLOAT,
+    },
+    quantidade:{
+        type: DataTypes.INTEGER,
+    },
+    createdAt:{
+        type: DataTypes.DATE,
+    },
+    updatedAt:{
+        type: DataTypes.DATE,
+    }
+
+},
+{
+    tableName: "produtos",
+}
+);
+
+module.exports = Produtos;
