7fa4f00f9921b718546c8b3d9bfe88bd2638fff7
Aline Mizumukai <aline.mizumukai@mag-it.com.br>
2022-11-14 20:32:46 -0300

    Atualizando consultar, deletar e atualizar produto

--- src/controllers/produtoController.js
+++ src/controllers/produtoController.js
@@ -6,23 +6,67 @@ const produtoController ={
 
         res.json(listaProdutos);
     },
+    
+    async consultarProduto(req, res){
+        const { id } = req.params;
+
+        const itemProdutos = await Produtos.findOne(
+            { 
+                where:{
+                    id,
+                },
+            }
+        );
+
+        res.json(itemProdutos);
+    },
 
     async cadastrarProduto(req, res){
         const { nome, preco, quantidade } = req.body;
 
-        const novoProduto = await Produtos.create({ 
-            nome, 
-            preco, 
-            quantidade
-        });
+        const novoProduto = await Produtos.create(
+            { 
+                nome, 
+                preco, 
+                quantidade
+            }
+        );
      
         res.json(novoProduto);
     },
 
-    consultarProduto(req, res){
-        console.log(req.params);
-        res.send("Hello Aline!");
-    }
+    async deletarProduto(req, res){
+        const { id } = req.params;
+
+        await Produtos.destroy(
+            { 
+                where:{
+                    id,
+                },
+            }
+        );
+
+        res.json("Produto deletado.")
+    },
+
+    async atualizarProduto(req, res){
+        const { id } = req.params;
+        const { nome, preco, quantidade } = req.body;
+
+        await Produtos.update(
+            { 
+                nome, 
+                preco, 
+                quantidade
+            },
+            {
+                where:{
+                    id,
+                },
+            }
+        );
+        res.json("Produto Atualizado");
+    },
 };
 
 module.exports = produtoController;
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,16 +2,10 @@ const express = require("express");
 const produtoController = require('../controllers/produtoController');
 const routes = express.Router();
 
-//rotas utilizam os verbos http
-routes.get("/", (req, res) =>{
-    console.log(req.query);
-    res.send("Hello Aline!");
-});
-
 routes.get("/produto/lista", produtoController.listarProduto);
-
-routes.get("/produto/:id", produtoController.consultarProduto);
-
+routes.get("/produto/:id/consultar", produtoController.consultarProduto);
 routes.post("/produto/criar", produtoController.cadastrarProduto);
+routes.delete("/produto/:id/deletar", produtoController.deletarProduto);
+routes.put("/produto/:id/atualizar", produtoController.atualizarProduto);
 
 module.exports = routes; // necessario para importar as rotas definidas aqui nos demais arquivos
