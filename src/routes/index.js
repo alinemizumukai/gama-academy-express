48c3f2ee28b780e3e269173360890f0fa8322342
Aline Mizumukai <aline.mizumukai@mag-it.com.br>
2022-11-28 14:03:36 -0300

    Adicionando middlewares

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const produtoController = require('../controllers/produtoController');
+const requestLog = require("../middlewares/requestLog");
+const bloqueio = require("../middlewares/bloqueio");
 const routes = express.Router();
 
-routes.get("/produtos", produtoController.retornarLista);
+routes.get("/produtos", requestLog, bloqueio, produtoController.retornarLista);
 routes.get("/produtos/:id", produtoController.retornarItem);
 routes.post("/produtos", produtoController.cadastrarItem);
 routes.delete("/produtos/:id", produtoController.deletarItem);
